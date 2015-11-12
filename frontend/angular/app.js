c12cff1a0df6a31e1c272a2a496de78eda776db7
gabrieldutra <nesk.frz@gmail.com>
2015-11-12 11:22:13 -0200

    Rotas

--- frontend/angular/app.js
+++ frontend/angular/app.js
@@ -1,12 +1,69 @@
-var app = angular.module('sistemaAcademico', []);
+var app = angular.module('sistemaAcademico', ['ngRoute', 'ngCookies']);
+
+app.config(function($routeProvider) {
+  
+  $routeProvider.when("/", 
+    {
+      templateUrl: "home.view.html",
+      controller: "HomeController",
+      controllerAs: "homeCtrl"
+    }
+  )
+  .when("/login", 
+    {
+      templateUrl: "login.view.html",
+      controller: "loginController",
+      controllerAs: "loginCtrl"
+    }
+  )
+  .otherwise(
+    {
+      redirectTo: "/"    
+    }
+  );
+});
 
 
-
-app.controller('loginController', ['$http',function($http) {
+app.factory('AuthenticationService', ['$http', '$cookieStore', '$rootScope', function($http, $cookieStore, $rootScope) {
+    var service = {};
+    
+    service.SetCredentials = SetCredentials;
+    service.ClearCredentials = ClearCredentials;
+    
+    return service;
+   
+        function SetCredentials(username, password) {
+            var authdata = username + ':' + password;
+ 
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    authdata: authdata
+                }
+            };
+ 
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+            $cookieStore.put('globals', $rootScope.globals);
+        }
+ 
+        function ClearCredentials() {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            $http.defaults.headers.common.Authorization = 'Basic';
+        }
+ 
+}]);
+    
+    
+app.controller('loginController', ['$rootScope','$location','$http', 'AuthenticationService',function($rootScope, $location, $http, AuthenticationService) {
     // acoes e propriedades do meu controller
     this.userLog = {};
     var self = this;
     self.logResp = {}; 
+    (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
     this.login = function(user) {
         /*
         var jsonObject = {'nome': 'Teste', 'login': 'Teste', 'senha': 'Teste', 'logradouro': 'Teste', 'numero': '100','bairro': 'Teste','cep': '102030','cidade': 'Teste','complemento': 'Teste','salario': '1000'};
@@ -16,13 +73,41 @@ app.controller('loginController', ['$http',function($http) {
             alert("nao funfou");
         });
         */
+       $("#btLogin").attr("disabled","disabled");
         $http.post('http://localhost:8000/backend/login',user).then(function(response){
             if(response.data.result === true){                
-                document.getElementById("response").innerHTML = "<p class='bg-success box'>Logado com sucesso como "+response.data.user.nome+"!</p>";
-            } else document.getElementById("response").innerHTML = "<p class='bg-danger box'>Login ou senha incorretos!</p>";
+                
+                $("#btLogin").text("Redirecionando...");
+                
+                AuthenticationService.SetCredentials(response.data.user.login, response.data.user.senha);
+       
+                document.getElementById("response").innerHTML = "<p class='alert alert-success box'>Logado com sucesso como "+$rootScope.globals.currentUser.username+"!</p>";
+  
+            } else {
+                document.getElementById("response").innerHTML = "<p class='alert alert-danger box'>Login ou senha incorretos!</p>";      
+                $("#btLogin").removeAttr("disabled");
+            }
             self.logResp=response.data;
         }, function(){
-            document.getElementById("response").innerHTML = "<p class='bg-danger box'>Erro ao tentar conectar banco de dados</p>";
+            document.getElementById("response").innerHTML = "<p class='bg-danger box'>Erro ao tentar conectar banco de dados</p>";            
+            $("#btLogin").removeAttr("disabled");
         });
     };
 }]);
+
+app.run(['$rootScope', '$location', '$cookieStore', '$http', function run($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+ 
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+    }]);
