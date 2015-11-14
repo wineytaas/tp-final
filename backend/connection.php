5d6c0a8ef463ff7f98660d29db6bee9a49cf7a1b
gabriel <gabriel@gabriel-pc>
2015-11-14 10:30:02 -0200

    Testando localhost

--- backend/connection.php
+++ backend/connection.php
@@ -3,9 +3,12 @@
 class Connection
 {
   public static $database = "daw-aluno2";
-  public static $address = "150.164.102.160";
-  public static $user = "daw-aluno2";
-  public static $password = "wisney";
+  //public static $address = "150.164.102.160";
+  public static $address = "localhost";
+  //public static $user = "daw-aluno2";
+  public static $user = "root";
+  public static $password = "allods6655";
+  //public static $password = "wisney";
 
   public static function getConnection() {
     $connection = mysqli_connect(Connection::$address, Connection::$user, Connection::$password, Connection::$database);
